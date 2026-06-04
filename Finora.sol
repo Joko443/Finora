eec8910b264ab20c218d17d63c1a645a8c87c12c
Joko Susilo <140326806+Joko443@users.noreply.github.com>
2026-06-04 12:30:46 +0800

    Update Finora.sol

--- Finora.sol
+++ Finora.sol
@@ -2,23 +2,15 @@
 pragma solidity ^0.8.20;
 
 contract Finora {
+    uint256 public total;
 
-```
-uint256 public vault;
+    function deposit() public {
+        total = total + 1;
+    }
 
-function deposit() public {
-    vault += 1;
-}
-
-function withdraw() public {
-    require(vault > 0, "Empty");
-
-    vault -= 1;
-}
-
-function getVault() public view returns (uint256) {
-    return vault;
-}
-```
-
+    function withdraw() public {
+        if (total > 0) {
+            total = total - 1;
+        }
+    }
 }
